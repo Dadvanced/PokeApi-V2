c0ffafa5cc475e81ec111e38d5a2e0d115d61e34
David León Galisteo <dadvanced@live.com>
2024-07-03 08:11:00 +0200

    first upload from local

---
+++ src/main/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        PokemonGUI.launch(PokemonGUI.class, args);
+    }
+}
---
+++ src/main/java/models/Ability.java
@@ -0,0 +1,34 @@
+package models;
+
+public class Ability {
+    private boolean isHidden;
+    private int slot;
+    private AbilityDetails ability;
+
+    public Ability() {
+    }
+
+    public boolean isHidden() {
+        return isHidden;
+    }
+
+    public void setHidden(boolean hidden) {
+        isHidden = hidden;
+    }
+
+    public int getSlot() {
+        return slot;
+    }
+
+    public void setSlot(int slot) {
+        this.slot = slot;
+    }
+
+    public AbilityDetails getAbility() {
+        return ability;
+    }
+
+    public void setAbility(AbilityDetails ability) {
+        this.ability = ability;
+    }
+}
