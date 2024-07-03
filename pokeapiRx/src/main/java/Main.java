0037bf07fdb0453261cfc936be2cff9d8b0dc361
David León Galisteo <dadvanced@live.com>
2024-07-03 08:39:32 +0200

    fixing folder errors

--- pokeapiRx/src/main/java/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        PokemonGUI.launch(PokemonGUI.class, args);
-    }
-}
--- pokeapiRx/src/main/java/utilities/Constants.java
+++
@@ -1,7 +0,0 @@
-package utilities;
-
-public class Constants {
-    public static final String BASE_URL = "https://pokeapi.co/api/v2";
-    public static final String POKEDEX_URL = BASE_URL + "/pokedex";
-    public static final String POKEMON_URL = BASE_URL + "/pokemon";
-}
